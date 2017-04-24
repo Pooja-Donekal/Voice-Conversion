61f54bc0afdfc408cde13c391dcf0ca56b824fa7
Pooja-Donekal <pdonek2@uic.edu>
2017-04-24 08:16:58 -0500

    Update ProjectFinal.py

--- ProjectFinal.py
+++ ProjectFinal.py
@@ -23,7 +23,7 @@ from os.path import isfile, join
 import dspUtil
 
 
-
+#reading the source and target audio files 
 sourcepath='source/'
 source=[]
 target=[]
@@ -47,21 +47,22 @@ count=0
 for sourcefile,targetfile in zip(source,target) :
     print(sourcefile,targetfile)
     sr, sx = wavfile.read(sourcefile)
-    sourceframes = librosa.util.frame(sx, frame_length=frameLength,
+    sourceframes = librosa.util.frame(sx, frame_length=frameLength,    #framing the source audio
     hop_length=hop_length).astype(np.float64).T
-    sourceframes *= pysptk.blackman(frameLength)
-    sourcemcepvectors = np.apply_along_axis(pysptk.mcep, 1, sourceframes, order, alpha)
+    sourceframes *= pysptk.blackman(frameLength) #windowing
+    sourcemcepvectors = np.apply_along_axis(pysptk.mcep, 1, sourceframes, order, alpha) #extract MCEPs of the source frames
     sr, tx = wavfile.read(targetfile)
-    targetframes = librosa.util.frame(tx, frame_length=frameLength,
+    targetframes = librosa.util.frame(tx, frame_length=frameLength,  #framing the target audio
     hop_length=hop_length).astype(np.float64).T
-    targetframes *= pysptk.blackman(frameLength)
-    targetmcepvectors = np.apply_along_axis(pysptk.mcep, 1, targetframes, order, alpha)
-    reslen=min(len(sourcemcepvectors),len(targetmcepvectors))
+    targetframes *= pysptk.blackman(frameLength) . #windowing
+    targetmcepvectors = np.apply_along_axis(pysptk.mcep, 1, targetframes, order, alpha) . #extract mceps of target frames
+    reslen=min(len(sourcemcepvectors),len(targetmcepvectors)) 
     transsourcemcepvectorsmod=np.empty([26,reslen])
     transtargetmcepvectorsmod=np.empty([26,reslen])
     transsourcemcepvectorsmod=np.transpose(sourcemcepvectors[0:reslen])
     transtargetmcepvectorsmod=np.transpose(targetmcepvectors[0:reslen])
     print(len(sourcemcepvectors),len(targetmcepvectors))
+    # to find if there are any NANs in the MCEP vectors 
     for i in range(len(sourcemcepvectors)):
         for j in range(len(sourcemcepvectors[i])):
             if np.isnan(sourcemcepvectors[i][j]):
@@ -71,28 +72,35 @@ for sourcefile,targetfile in zip(source,target) :
             if np.isnan(targetmcepvectors[i][j]):
                 print("no")
     print("Before",net["w"])
+    #training the neural network
     net=pyrenn.train_LM(transsourcemcepvectorsmod,transtargetmcepvectorsmod,net,k_max=1000,verbose=True,E_stop=5)
     print("After",net["w"])
     count=count+1
 
-
+#testing source file
 sourcepath='source'
 sourcefile='arctic_a0018.wav'    
 sr, sx = wavfile.read(sourcepath+'/'+sourcefile)
 l=len(sx)
 
+#framing 
 sourceframes = librosa.util.frame(sx, frame_length=frameLength,
 hop_length=hop_length).astype(np.float64).T
 
 # Windowing
 sourceframes*= pysptk.blackman(frameLength)
 
+#extract MCEPs
 sourcemcepvectors = np.apply_along_axis(pysptk.mcep, 1, sourceframes, order, alpha) 
 mgc=np.empty([len(sourcemcepvectors),26])
+#provide the source MCEPs as input to the trained neural network which gives the target MCEPs
 mgc=pyrenn.NNOut(sourcemcepvectors.transpose(),net).transpose()
 mgc=mgc.copy(order="C")
+#apply mgc2sp to get spectral estimation from MCEPs
 logHmgc = np.apply_along_axis(pysptk.mgc2sp, 1, mgc, 0.41, 0.0, frameLength)
+#inverse log operation to obtain audio in frequency domain 
 rawHmgc=np.exp(logHmgc).T
+# Inverse fourier transform to convert to time domain
 mgcRecover = librosa.core.istft(rawHmgc, hop_length, frameLength,
                                     pysptk.blackman(frameLength))
 Audio(mgcRecover, rate=sr)
